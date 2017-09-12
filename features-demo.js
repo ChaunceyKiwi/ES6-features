f7ea2c2e3bcec352e25a1a88bdeb2d4b0a105986
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-09-11 21:37:21 -0700

    demo on new built-in methods added

--- features-demo.js
+++ features-demo.js
@@ -250,7 +250,53 @@
 //     console.log(key + " = " + val);
 // }
 
+/* Object Property Assignment */
+let dest = {quux: 0};
+var src1 = {foo: 1, bar: 2};
+var src2 = {foo: 3, baz: 4};
+Object.assign(dest, src1, src2);
+console.log(dest.quux === 0);
+console.log(dest.foo === 3);
+console.log(dest.bar === 2);
+console.log(dest.baz === 4);
 
+/* Array Element Finding */
+console.log([1, 3, 4, 2].find(x => x > 3));
+console.log([1, 3, 4, 2].findIndex(x => x > 3));
+
+/* String Repeating */
+console.log("foo".repeat(3));
+
+/* String Searching */
+console.log("hello".startsWith("ello", 1));
+console.log("hello".endsWith("hell", 4));
+console.log("hello".includes("ell"));
+console.log("hello".includes("ell", 1));
+console.log("hello".includes("ell", 2) == false);
+
+/* Number Type Checking */
+console.log(Number.isNaN(42) === false);
+console.log(Number.isNaN(NaN) === true);
+console.log(Number.isFinite(Infinity) === false);
+console.log(Number.isFinite(-Infinity) === false);
+console.log(Number.isFinite(NaN) === false);
+console.log(Number.isFinite(123) === true);
+
+/* Number Comparison */
+console.log(0.1 + 0.2 === 0.3);
+console.log(Math.abs((0.1 + 0.2) - 0.3) < Number.EPSILON);
+
+/* Number Truncation */
+console.log(Math.trunc(42.7));
+console.log(Math.trunc(0.1));
+console.log(Math.trunc(-0.1));
+
+/* Number Sign Determination */
+console.log(Math.sign(7));
+console.log(Math.sign(0));
+console.log(Math.sign(-0));
+console.log(Math.sign(-7));
+console.log(Math.sign(NaN));
 
 /************************ Other ******************************/
 // /* Type conversion */
