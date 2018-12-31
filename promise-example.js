599153de1253152efc02db63b88db642bfea55ef
Chauncey <chaunceykiwi@gmail.com>
2018-12-31 15:04:17 -0800

    Update promise-example.js

--- promise-example.js
+++ promise-example.js
@@ -1,9 +1,18 @@
+/* 
+ * The function passed to `new Promise` is called executor. When the 
+ * promise is created, this executor function runs automatically. 
+ *
+ * The executor received two arguments: `resolve` and `reject`. These two
+ * functions aer pre-defined by the JavaScript engine. We do not need to
+ * create them. Instead we should write the executor to call them when ready.
+ */
+
 let promise = new Promise(function(resolve, reject) {
   /* The producing code spend 3 seconds running and successfully return a result */
-  // setTimeout(() => resolve("done!"), 1000);
+  // setTimeout(() => resolve("done!"), 3000);
 
   /* The producing code spend 3 seconds running and return a error code */
-  setTimeout(() => reject(new Error("Whoops!")), 1000);
+  setTimeout(() => reject(new Error("Whoops!")), 3000);
 });
 
 promise.then(
